cb43bb00e823b02e7e3b245417f345842a7999b6
Hiếu Lê Minh <leminhhieu1210@gmail.com>
2020-07-20 18:00:28 +0700

    demo lan hai

---
+++ linhtinh.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	long long n;
+	
+	scanf("%lld", &n);
+	
+	for (long long i = 2; i * i <= n; ++i)
+	{
+		while (n % (i*i) == 0) 
+		{
+			printf("n = %lld\ti = %lld\n", n, i);
+			n /= i;
+		}
+	}
+	
+	printf("%lld", n);
+	
+	return 0;
+}
+
